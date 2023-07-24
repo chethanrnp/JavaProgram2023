07d88b751a502e7921dbeac841f8470df44c7e05
chethanrnp <chethan.supernova@gmail.com>
2023-07-24 09:32:27 +0530

    Changes

--- src/test/java/SpecJavaProgram/Practise_1.java
+++ src/test/java/SpecJavaProgram/Practise_1.java
@@ -4,5 +4,8 @@ public class Practise_1 {
 
 	public static void main(String[] args) {
 		System.out.println("i am from git");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("name");
+		}
 	}
 }
